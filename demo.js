b688065b662311453825ce871a3a9595599412e7
nishith004 <nishith004@gmail.com>
2024-09-06 02:30:46 +0530

    First commit

---
+++ demo.js
@@ -0,0 +1,16 @@
+console.log("Welcome to JavaScript");
+
+// number examples
+var num1 = 10;
+var num2 = 20;
+
+console.log("The value of num1 is - " + num1);
+console.log("The value of num1 is - " + num2);
+
+// String exaples
+
+var fname = "Nishith";
+var lname = "Trivedi";
+
+console.log("Firs name is " + fname);
+console.log("Last name is " + lname);
